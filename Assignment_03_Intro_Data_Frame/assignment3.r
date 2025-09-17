76cc934335d2443565a113cd11c1858d8671d5db
agl100 <aglane@usf.edu>
2025-09-17 14:50:19 -0400

    Update and rename assignment_03 (organizing folders)

--- Assignment_03_Intro_Data_Frame/assignment3.r
+++ Assignment_03_Intro_Data_Frame/assignment3.r
@@ -33,6 +33,4 @@ df_long <- rbind(
 ggplot(df_long, aes(x = Name, y = Value, fill = Poll)) +
   geom_col(position = "dodge") +
   labs(title = "Poll Results by Candidate",
-       x = "Candidate", y = "Value")
-
-# link to blog: https://rprogrammingfall25.blogspot.com/2025/09/module-3-assignment-data-structures-in-r.html 
+       x = "Candidate", y = "Value") 
